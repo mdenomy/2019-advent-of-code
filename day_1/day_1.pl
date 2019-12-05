2719357453bfea76c0a65e480ec1c7540772171e
Michael Denomy <mdenomy@gmail.com>
2019-12-05 15:21:46 -0500

    Part 2 solution

--- day_1/day_1.pl
+++ day_1/day_1.pl
@@ -2,11 +2,29 @@ use POSIX qw(floor);
 
 open(my $in,  "<",  "input.txt")  or die "Can't open input.txt: $!";
 
-my $total = 0;
+my $part1_fuel_required = 0;
+my $part2_fuel_required = 0;
 
+# Could have read the whole file in, but do it line by line in case of huge file
 while (<$in>) {
-     $fuel_required = floor($_ / 3) - 2;
-     $total += $fuel_required;
+    $part1_fuel_required += calculate_fuel($_);
+    $part2_fuel_required += calculate_total_fuel($_);
  }
 
-print "Total fuel required = $total\n";
+print "Part 1 Total fuel required = $part1_fuel_required\n";
+print "Part 2 Total fuel required = $part2_fuel_required\n";
+
+# Calculates the fuel for a given mass
+sub calculate_fuel {
+	return floor($_[0] / 3) - 2;
+}
+
+# Calculates the fuel for a given mass, plus the mass of the fuel
+sub calculate_total_fuel {
+	my $fuel = calculate_fuel($_[0]);
+	if ($fuel > 0) {
+		return $fuel += calculate_total_fuel($fuel);
+	} else {
+		return 0;
+	}
+}
