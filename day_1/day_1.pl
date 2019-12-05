5bedd4b9f6ab3ba0fadbe6984532219a930e714a
Michael Denomy <mdenomy@Michaels-MacBook-Pro.local>
2019-12-05 08:47:27 -0500

    Fixed some odd spacing

--- day_1/day_1.pl
+++ day_1/day_1.pl
@@ -1,6 +1,6 @@
- use POSIX qw(floor);
+use POSIX qw(floor);
 
- open(my $in,  "<",  "input.txt")  or die "Can't open input.txt: $!";
+open(my $in,  "<",  "input.txt")  or die "Can't open input.txt: $!";
 
 my $total = 0;
 
@@ -9,4 +9,4 @@ while (<$in>) {
      $total += $fuel_required;
  }
 
- print "Total fuel required = $total\n";
+print "Total fuel required = $total\n";
